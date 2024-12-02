6fe805765d63229235351b05d43563a52f14eb9e
Joe Griffin <wackro@gmail.com>
2024-12-02 00:23:43 +0000

    Do part 2

---
+++ Day1.Part2/Program.cs
@@ -0,0 +1,33 @@
+﻿var occurences1 = new Dictionary<int, int>() ;
+var occurences2 = new Dictionary<int, int>();
+
+using var s = new StreamReader("./input");
+while (s.ReadLine() is { } line)
+{
+    PopulateOccurences(occurences1, int.Parse(line[..5]));
+    PopulateOccurences(occurences2, int.Parse(line[8..]));
+}
+
+int total = 0;
+foreach(var o in occurences1)
+{
+    if(occurences2.TryGetValue(o.Key, out var o2Score))
+    {
+        total += o.Key * o.Value * o2Score;
+    }
+}
+
+Console.WriteLine(total);
+Console.ReadLine();
+
+static void PopulateOccurences(Dictionary<int, int> occurences, int l1Value)
+{
+    if (!occurences.TryGetValue(l1Value, out int value))
+    {
+        occurences.Add(l1Value, 1);
+    }
+    else
+    {
+        occurences[l1Value] = ++value;
+    }
+}
